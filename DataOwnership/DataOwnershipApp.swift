970af95cf6c2ff0aa87ef84620c2e169afa973ee
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2023-06-17 21:48:59 +0300

    Initial Commit

---
+++ DataOwnership/DataOwnershipApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataOwnershipApp.swift
+//  DataOwnership
+//
+//  Created by Anastasia Holovash on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataOwnershipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
